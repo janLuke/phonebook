c4fb597c2f7af262899cc36cbd7e3840357d37c9
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-12-10 01:45:44 +0100

    [3.3] Implement GET api/contacts/:id

--- server/index.js
+++ server/index.js
@@ -5,16 +5,29 @@ const PORT = 3001
 
 app = express()
 
-app.get('/api/contacts', (req, res) => {
-   res.json(contacts);
-})
-
 app.get('/info', (req, res) => {
    let now = new Date()
    res.send(`
       <p>Phonebook has ${contacts.length} contacts.</p>
       ${now}
    `)
+})
+
+app.get('/api/contacts', (req, res) => {
+   res.json(contacts);
+})
+
+app.get('/api/contacts/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   if (isNaN(id))
+      return res.status(400).json({
+         error: 'the <id> parameter should be an integer'
+      })
+   let contact = contacts.find(c => c.id == id)
+   if (contact != null)
+      return res.json(contact)
+   else
+      return res.status(404).end()
 })
 
 app.listen(PORT, () => {
